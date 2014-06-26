c581990478bccafd4c6f38afba9dbc5ca60062f4
Thomas Gorissen <serrynaimo@me.com>
2014-06-26 13:24:20 +0800

    Reversing to old tests

--- tests/adapter_test.js
+++ tests/adapter_test.js
@@ -4,5 +4,10 @@ var test = require('tape'),
 
 test('RTCPeerConnection is defined', function (t) {
   t.plan(1);
-  t.pass();
+  t.notEqual(window.RTCPeerConnection, undefined);
+});
+
+test('getUserMedia is defined', function (t) {
+  t.plan(1);
+  t.notEqual(window.getUserMedia, undefined);
 });
