d5bead91a14feb15ed556e065d855fecb80a80f2
Leticia Choo <leticiachoo@Leticias-MacBook-Pro.local>
2014-09-17 19:09:25 +0800

    Updated test script.

--- tests/adapter_test.js
+++ tests/adapter_test.js
@@ -1,13 +1,42 @@
 var test = require('tape'),
-	adapter = require('./../publish/adapter.min.js');
+  adapter = require('./../publish/adapter.min.js');
 
-
+// Check if function is defined
 test('RTCPeerConnection is defined', function (t) {
   t.plan(1);
-  t.notEqual(window.RTCPeerConnection, undefined);
+  t.equal(typeof window.RTCPeerConnection, 'function');
+});
+
+test('RTCSessionDescription is defined', function (t) {
+  t.plan(1);
+  t.equal(typeof window.RTCSessionDescription, 'function');
+});
+
+test('RTCIceCandidate is defined', function (t) {
+  t.plan(1);
+  t.equal(typeof window.RTCIceCandidate, 'function');
 });
 
 test('getUserMedia is defined', function (t) {
   t.plan(1);
-  t.notEqual(window.getUserMedia, undefined);
+  t.equal(typeof window.getUserMedia, 'function');
+});
+
+test('attachMediaStream is defined', function (t) {
+  t.plan(1);
+  t.equal(typeof window.attachMediaStream, 'function');
+});
+
+test('reattachMediaStream is defined', function (t) {
+  t.plan(1);
+  t.equal(typeof window.reattachMediaStream, 'function');
+});
+
+test('MediaStreamTrack.getSources', function (t) {
+  t.plan(1);
+  window.MediaStreamTrack.getSources(function (sources) {
+    if (sources instanceof Array) {
+      t.pass();
+    }
+  });
 });
