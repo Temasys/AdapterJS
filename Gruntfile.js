81bf8c20d650da689690ef269e4219e8730600d2
Thomas Gorissen <serrynaimo@me.com>
2015-01-15 18:37:06 +0800

    Preparing for bamboo building

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function (grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -16,9 +17,33 @@ module.exports = function (grunt) {
 
 		production: 'publish',
 
+        bamboo: 'bamboo',
+
 		clean: {
-			production: ['<%= production %>/']
+			production: ['<%= production %>/'],
+			bamboo: ['<%= bamboo %>/']
 		},
+
+        copy: {
+            bamboo: {
+                files: [{
+                	expand: true,
+                	cwd: '<%= production %>/',
+                    src: ['**'],
+                    dest: '<%= bamboo %>/adapterjs/<%= pkg.version %>'
+                }, {
+                	expand: true,
+                	cwd: '<%= production %>/',
+                    src: ['**'],
+                    dest: '<%= bamboo %>/adapterjs/<%= pkg.version_major %>.<%= pkg.version_minor %>.x'
+                }, {
+                	expand: true,
+                	cwd: '<%= production %>/',
+                    src: ['**'],
+                    dest: '<%= bamboo %>/adapterjs/latest'
+                }],
+            },
+        },
 
 		concat: {
 			options: {
@@ -43,7 +68,7 @@ module.exports = function (grunt) {
 				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
 					'<%= grunt.template.today("yyyy-mm-dd") %> */\n'
 			},
-			production_min: {
+			production: {
 				files: {
 					'<%= production %>/adapter.min.js': ['<%= production %>/adapter.debug.js']
 				}
@@ -51,7 +76,7 @@ module.exports = function (grunt) {
 		},
 
 		replace: {
-			dist: {
+			production: {
 				options: {
 					variables: {
 						'version': '<%= pkg.version %>'
@@ -103,10 +128,77 @@ module.exports = function (grunt) {
 		}
 	});
 
+
+    grunt.registerTask('versionise', 'Adds version meta intormation', function() {
+        var done = this.async(),
+            arr = [];
+
+        grunt.util.spawn({
+            cmd: 'git',
+            args: ['log', '-1', '--pretty=format:%h\n %ci']
+        }, function(err, result) {
+            if (err) {
+                return done(false);
+            }
+            arr = result.toString().split('\n ');
+            grunt.config('meta.rev', arr[0]);
+            grunt.config('meta.date', arr[1]);
+        });
+
+        try {
+            var version = grunt.config('pkg.version').match('^([0-9]+)\.([0-9]+)\.([0-9]+)$')
+            grunt.config('pkg.version_major', version[1]);
+            grunt.config('pkg.version_minor', version[2]);
+            grunt.config('pkg.version_release', version[3]);
+        }
+        catch (e) {
+        	grunt.fatal('Version ' + grunt.config('pkg.version') + ' has not the correct format.');
+        }
+
+        grunt.util.spawn({
+            cmd: 'git',
+            args: [
+                'for-each-ref',
+                '--sort=*authordate',
+                '--format="%(tag)"',
+                'refs/tags'
+            ]
+        }, function(err, result) {
+            if (err) {
+                return done(false);
+            }
+            arr = result.toString().split('\n');
+
+            var tag = arr[arr.length - 1];
+            tag = tag.toString();
+            grunt.config('meta.tag', tag);
+
+            grunt.log.write('Version: ' + grunt.config('pkg.version') +
+            	'\nRevision: ' + grunt.config('meta.rev') +
+            	'\nDate: ' + grunt.config('meta.date') +
+            	'\nGit Tag: ' + grunt.config('meta.tag') + '\n');
+
+            done(result);
+        });
+    });
+
+	grunt.registerTask('bamboovars', 'Write bamboo variables to file', function() {
+		grunt.file.write('bamboo/vars', 'version=' + grunt.config('pkg.version'));
+		grunt.log.writeln('bamboo/vars file successfully created');
+	});
+
 	grunt.registerTask('publish', [
+		'versionise',
 		'clean:production',
-		'concat:production',
-		'replace:dist',
-		'uglify:production_min'
+		'concat',
+		'replace',
+		'uglify'
+	]);
+
+	grunt.registerTask('bamboo', [
+		'publish',
+		'clean:bamboo',
+		'copy',
+		'bamboovars'
 	]);
 };
