5d5712b4974f3ecb30cf18ea14ade1719e4d6f71
Leticia Choo <leticia.choo@temasys.com.sg>
2015-04-17 11:50:02 +0800

    AJS-29 AJS-74 #comment Updated test for RTCPeerConnection parameters and interface extensions parameters tests.

--- tests/unit/RTCPeerConnection.constraints.spec.js
+++ tests/unit/RTCPeerConnection.constraints.spec.js
@@ -14,160 +14,9 @@ var gUMTimeout = 25000;
 // Test item timeout
 var testItemTimeout = 2000;
 
-// Parse the constraints of getUserMedia
-var printJSON = function (obj, spaces) {
-  spaces = typeof spaces !== 'number' ? 2 : spaces;
-
-  // make indentation
-  var makeIndentation = function (spaces) {
-    var str = '';
-    var i;
-
-    for (i = 0; i < spaces; i += 1) {
-      str += ' ';
-    }
-
-    return str;
-  };
-
-  var opening = '{';
-  var closing = '}';
-
-  if (obj instanceof Array) {
-    opening = '[';
-    closing = ']';
-  }
-
-  // parse object
-  var outputStr = makeIndentation(spaces - 2) + opening;
-  var val;
-
-
-  if (!(obj instanceof Array)) {
-    var key;
-
-    for (key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        outputStr += '\n\t' + makeIndentation(spaces) + '"' + key + '": ';
-
-        val = obj[key];
-
-        if (typeof val === 'object') {
-          outputStr += printJSON(val, spaces + 2);
-
-        } else if (typeof val === 'string') {
-          outputStr += '"' + val + '"';
-
-        } else {
-          outputStr += val;
-        }
-
-        outputStr += ',';
-      }
-    }
-  } else {
-    var i;
-
-    for (i = 0; i < obj.length; i += 1) {
-      val = obj[i];
-
-      if (typeof val === 'object') {
-        outputStr += printJSON(val, spaces + 2);
-
-      } else if (typeof val === 'string') {
-        outputStr += '"' + val + '"';
-
-      } else {
-        outputStr += val;
-      }
-
-      if (i < (obj.length - 1)) {
-        outputStr += ',';
-      }
-    }
-  }
-
-  outputStr += '\n\t' + makeIndentation(spaces - 2) + closing;
-
-  return outputStr;
-};
-
 
 describe('RTCPeerConnection | RTCConfiguration', function() {
   this.timeout(testTimeout);
-
-  var tc1Constraints = {
-    iceServers: []
-  };
-
-  var tc2Constraints = {
-    iceServers: [{
-      url: 'turn:numb.viagenie.ca',
-      username: 'leticia.choo@temasys.com.sg',
-      credential: 'xxxxx'
-    }]
-  };
-
-  var tc3Constraints = {
-    iceServers: [{
-      url: 'leticia.choo@temasys.com.sg@turn:numb.viagenie.ca',
-      credential: 'xxxxx'
-    }]
-  };
-
-  var tc4Constraints = {
-    iceServers: [{
-      urls: ['turn:numb.viagenie.ca', 'turn:numb.viagenie.ca'],
-      username: 'leticia.choo@temasys.com.sg',
-      credential: 'xxxxx'
-    }]
-  };
-
-  var tc5Constraints = {
-    iceServers: [{
-      url: 'stun:stun.l.google.com:19302'
-    }]
-  };
-
-  var tc6Constraints = {
-    iceServers: [{
-      url: 'turn:numb.viagenie.ca',
-      username: 'leticia.choo@temasys.com.sg',
-      credential: 'xxxxx'
-    }, {
-      url: 'stun:stun.l.google.com:19302'
-    }]
-  };
-
-  var tc7Constraints = {
-    bundlePolicy: 'balanced'
-  };
-
-  var tc8Constraints = {
-    bundlePolicy: 'max-compat'
-  };
-
-  var tc9Constraints = {
-    bundlePolicy: 'max-bundle'
-  };
-
-  var tc10Constraints = {
-    iceTransportPolicy: 'none'
-  };
-
-  var tc11Constraints = {
-    iceTransportPolicy: 'relay'
-  };
-
-  var tc12Constraints = {
-    iceTransportPolicy: 'all'
-  };
-
-  var tc13Constraints = {
-    optional: [{
-      DtlsSrtpKeyAgreement: true
-    }]
-  };
 
   /* WebRTC Object should be initialized in Safari/IE Plugin */
   before(function (done) {
@@ -187,58 +36,121 @@ describe('RTCPeerConnection | RTCConfiguration', function() {
     }
   });
 
+  (function (constraints) {
 
-  it('new RTCPeerConnection(' + printJSON(tc1Constraints) + ')', function () {
-    this.timeout(testItemTimeout);
+    it('new RTCPeerConnection(' + printJSON(constraints) + ')', function () {
+      this.timeout(testItemTimeout);
 
-    var peer = new RTCPeerConnection(tc1Constraints);
-  });
+      var peer = new RTCPeerConnection(constraints);
+    });
 
-  it('new RTCPeerConnection(' + printJSON(tc2Constraints) + ')', function () {
-    this.timeout(testItemTimeout);
+  })({ iceServers: [] });
 
-    var peer = new RTCPeerConnection(tc2Constraints);
-  });
 
-  it('new RTCPeerConnection(' + printJSON(tc3Constraints) + ')', function () {
-    this.timeout(testItemTimeout);
+  (function (constraints) {
 
-    var peer = new RTCPeerConnection(tc3Constraints);
-  });
+    it('new RTCPeerConnection(' + printJSON(constraints) + ')', function () {
+      this.timeout(testItemTimeout);
 
-  it.skip('new RTCPeerConnection(' + printJSON(tc4Constraints) + ')', function () {
-    this.timeout(testItemTimeout);
+      var peer = new RTCPeerConnection(constraints);
+    });
 
-    var peer = new RTCPeerConnection(tc2Constraints);
-  });
+  })({ iceServers: [{ url: 'turn:numb.viagenie.ca', username: 'leticia.choo@temasys.com.sg', credential: 'xxxxx' }] });
 
-  it.skip('new RTCPeerConnection(' + printJSON(tc5Constraints) + ')', function () {
-    this.timeout(testItemTimeout);
 
-    var peer = new RTCPeerConnection(tc2Constraints);
-  });
+  (function (constraints) {
 
-  it.skip('new RTCPeerConnection(' + printJSON(tc6Constraints) + ')', function () {
-    this.timeout(testItemTimeout);
+    it('new RTCPeerConnection(' + printJSON(constraints) + ')', function () {
+      this.timeout(testItemTimeout);
 
-    var peer = new RTCPeerConnection(tc2Constraints);
-  });
+      var peer = new RTCPeerConnection(constraints);
+    });
 
-  it.skip('new RTCPeerConnection(' + printJSON(tc7Constraints) + ')', function () {});
+  })({ iceServers: [{ url: 'leticia.choo@temasys.com.sg@turn:numb.viagenie.ca', credential: 'xxxxx' }] });
 
-  it.skip('new RTCPeerConnection(' + printJSON(tc8Constraints) + ')', function () {});
 
-  it.skip('new RTCPeerConnection(' + printJSON(tc9Constraints) + ')', function () {});
+  (function (constraints) {
 
-  it.skip('new RTCPeerConnection(' + printJSON(tc10Constraints) + ')', function () {});
+    it('new RTCPeerConnection(' + printJSON(constraints) + ')', function () {
+      this.timeout(testItemTimeout);
 
-  it.skip('new RTCPeerConnection(' + printJSON(tc11Constraints) + ')', function () {});
+      var peer = new RTCPeerConnection(constraints);
+    });
 
-  it.skip('new RTCPeerConnection(' + printJSON(tc12Constraints) + ')', function () {});
+  })({ iceServers: [{ urls: ['turn:numb.viagenie.ca', 'turn:numb.viagenie.ca'], username: 'leticia.choo@temasys.com.sg', credential: 'xxxxx' }] });
 
-  it('new RTCPeerConnection(' + printJSON(tc1Constraints) + ', ' + printJSON(tc13Constraints) + ')', function () {
-    this.timeout(testItemTimeout);
 
-    var peer = new RTCPeerConnection(tc1Constraints, tc10Constraints);
-  });
+  (function (constraints) {
+
+    it('new RTCPeerConnection(' + printJSON(constraints) + ')', function () {
+      this.timeout(testItemTimeout);
+
+      var peer = new RTCPeerConnection(constraints);
+    });
+
+  })({ iceServers: [{ url: 'stun:stun.l.google.com:19302' }] });
+
+
+  (function (constraints) {
+
+    it('new RTCPeerConnection(' + printJSON(constraints) + ')', function () {
+      this.timeout(testItemTimeout);
+
+      var peer = new RTCPeerConnection(constraints);
+    });
+
+  })({ iceServers: [{ url: 'turn:numb.viagenie.ca', username: 'leticia.choo@temasys.com.sg', credential: 'xxxxx' }, { url: 'stun:stun.l.google.com:19302' }] });
+
+
+  (function (constraints) {
+
+    it.skip('new RTCPeerConnection(' + printJSON(constraints) + ')', function () {});
+
+  })({ bundlePolicy: 'balanced' });
+
+
+  (function (constraints) {
+
+    it.skip('new RTCPeerConnection(' + printJSON(constraints) + ')', function () {});
+
+  })({ bundlePolicy: 'max-compat' });
+
+
+  (function (constraints) {
+
+    it.skip('new RTCPeerConnection(' + printJSON(constraints) + ')', function () {});
+
+  })({ bundlePolicy: 'max-bundle' });
+
+
+  (function (constraints) {
+
+    it.skip('new RTCPeerConnection(' + printJSON(constraints) + ')', function () {});
+
+  })( iceTransportPolicy: 'none' });
+
+
+  (function (constraints) {
+
+    it.skip('new RTCPeerConnection(' + printJSON(constraints) + ')', function () {});
+
+  })({ iceTransportPolicy: 'relay' });
+
+
+  (function (constraints) {
+
+    it.skip('new RTCPeerConnection(' + printJSON(constraints) + ')', function () {});
+
+  })({ iceTransportPolicy: 'all' });
+
+
+  (function (constraints, optional) {
+
+    it('new RTCPeerConnection(' + printJSON(constraints) + ', ' + printJSON(optional) + ')', function () {
+      this.timeout(testItemTimeout);
+
+      var peer = new RTCPeerConnection(constraints, optional);
+    });
+
+  })({ iceServers: [] }, { optional: [{ DtlsSrtpKeyAgreement: true }] });
 });
