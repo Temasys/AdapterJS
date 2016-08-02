cc31d563a458766ce9c59d6ca57b9fe12afce2a0
Pete Winterscheidt <pete.winterscheidt@readytalk.com>
2016-08-02 11:17:02 -0600

    Allow for pluginInfo overrides, include downloadlinks as part of pluginInfo.

--- source/pluginInfo.js
+++ source/pluginInfo.js
@@ -1,4 +1,4 @@
-AdapterJS.WebRTCPlugin.pluginInfo = {
+AdapterJS.WebRTCPlugin.pluginInfo = AdapterJS.WebRTCPlugin.pluginInfo || {
   prefix : 'Tem',
   plugName : 'TemWebRTCPlugin',
   pluginId : 'plugin0',
@@ -6,11 +6,15 @@ AdapterJS.WebRTCPlugin.pluginInfo = {
   onload : '__TemWebRTCReady0',
   portalLink : 'http://skylink.io/plugin/',
   downloadLink : null, //set below
-  companyName: 'Temasys'
+  companyName: 'Temasys',
+  downloadLinks : {
+    mac: 'http://bit.ly/1n77hco',
+    win: 'http://bit.ly/1kkS4FN'
+  }
 };
 if(!!navigator.platform.match(/^Mac/i)) {
-  AdapterJS.WebRTCPlugin.pluginInfo.downloadLink = 'http://bit.ly/1n77hco';
+  AdapterJS.WebRTCPlugin.pluginInfo.downloadLink = AdapterJS.WebRTCPlugin.pluginInfo.downloadLinks.mac || 'http://bit.ly/1n77hco';
 }
 else if(!!navigator.platform.match(/^Win/i)) {
-  AdapterJS.WebRTCPlugin.pluginInfo.downloadLink = 'http://bit.ly/1kkS4FN';
+  AdapterJS.WebRTCPlugin.pluginInfo.downloadLink = AdapterJS.WebRTCPlugin.pluginInfo.downloadLinks.win || 'http://bit.ly/1kkS4FN';
 }
