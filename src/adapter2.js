6846715617e17bd7e383828a7f2b719b41ed121e
Jacques-Olivier Haché <jo.hache@temasys.io>
2019-09-26 12:24:17 +0800

    NLC

--- src/adapter2.js
+++ src/adapter2.js
@@ -8,8 +8,8 @@ import adapter from 'webrtc-adapter/dist/adapter_core';
 // import * as config          from './config';
 import * as utils                     from './utils';
 import * as native_attachmediastream  from './attachmediastream';
-import * as pluginManager             from './plugin/plugin_manager';
-import * as getusermedia              from './plugin/getusermedia';
+import * as plugin_manager            from './plugin/plugin_manager';
+import * as plugin_getusermedia       from './plugin/getusermedia';
 import * as plugin_attachmediastream  from './plugin/attachmediastream';
 import * as plugin_peerConnection     from './plugin/peerconnection.js';
 import * as plugin_rtpSender          from './plugin/rtpsender.js';
@@ -21,24 +21,25 @@ let browserDetails = utils.detectBrowser(window);
 
 if ( browserDetails.browser == 'IE'
   || browserDetails.browser == 'safari-plugin' ) {
-  console.log('injectPlugin');
-  let pageId         = Math.random().toString(36).slice(2);
-  pluginManager.init(window, pageId);
-  pluginManager.injectPlugin();
-  getusermedia.shimGetUserMedia(window, pluginManager);
-  plugin_attachmediastream.shimAttachMediaStream(window, pluginManager, pageId);
-  plugin_peerConnection.shimPeerConnection(window, pluginManager);
-  plugin_rtpSender.shimRTCRtpSender(window, pluginManager);
-  plugin_rtpReceiver.shimRTCRtpReceiver(window, pluginManager);
-  plugin_rtpTransceiver.shimRTCRtpTransceiver(window, pluginManager);
-} else {
+  console.log('Using Temasys Plugin');
+  let pageId = Math.random().toString(36).slice(2);
+  plugin_manager.init(window, pageId);
+  plugin_manager.injectPlugin();
+  plugin_getusermedia.shimGetUserMedia(window, plugin_manager);
+  plugin_attachmediastream.shimAttachMediaStream(window, plugin_manager, pageId);
+  plugin_peerConnection.shimPeerConnection(window, plugin_manager);
+  plugin_rtpSender.shimRTCRtpSender(window, plugin_manager);
+  plugin_rtpReceiver.shimRTCRtpReceiver(window, plugin_manager);
+  plugin_rtpTransceiver.shimRTCRtpTransceiver(window, plugin_manager);
+} 
+else {
   native_attachmediastream.shimAttachMediaStream(window);
 }
 
-
 let AdapterJS = {
   webrtcAdapter: adapter,
-  webRTCReady: pluginManager.webRTCReady
+  webRTCReady: plugin_manager.webRTCReady,
+  pluginManager: plugin_manager,
 };
 
 window.AdapterJS = AdapterJS
